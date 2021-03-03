6ad87141d480935d35af4532d5d6f4ecd36a9414
Sergey Vakhramov <vsa8813@gmail.com>
2021-03-03 17:07:09 +0500

    feat(components): add switch btn

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,52 +1,59 @@
 import React from 'react';
 
+import SwitchIcon from '../../assets/images/icons/arrows.svg';
+
 import './SearchForm.scss';
 
 const SearchForm = (): JSX.Element => {
   return (
-    <form className="search-form">
-      <label htmlFor="origin" className="search-form__label">
-        <input
-          id="origin"
-          type="text"
-          placeholder="Откуда"
-          className="search-form__input search-form__input--origin"
-        />
-      </label>
+    <div className="search-form-wrapper">
+      <form className="search-form">
+        <label htmlFor="origin" className="search-form__label relative">
+          <input
+            id="origin"
+            type="text"
+            placeholder="Откуда"
+            className="search-form__input search-form__input--origin"
+          />
+          <button type="button" className="switch-btn">
+            <SwitchIcon />
+          </button>
+        </label>
 
-      <label htmlFor="destination" className="search-form__label ">
-        <input
-          id="destination"
-          type="text"
-          placeholder="Куда"
-          className="search-form__input search-form__input--destination"
-        />
-      </label>
+        <label htmlFor="destination" className="search-form__label ">
+          <input
+            id="destination"
+            type="text"
+            placeholder="Куда"
+            className="search-form__input search-form__input--destination"
+          />
+        </label>
 
-      <label htmlFor="depart" className="search-form__label ">
-        <input
-          id="depart"
-          type="text"
-          placeholder="Отправление"
-          className="search-form__input search-form__input--depart"
-        />
-      </label>
+        <label htmlFor="depart" className="search-form__label ">
+          <input
+            id="depart"
+            type="text"
+            placeholder="Отправление"
+            className="search-form__input search-form__input--depart"
+          />
+        </label>
 
-      <label htmlFor="return" className="search-form__label ">
-        <input
-          id="return"
-          type="text"
-          placeholder="Прибытие"
-          className="search-form__input search-form__input--return"
-        />
-      </label>
+        <label htmlFor="return" className="search-form__label ">
+          <input
+            id="return"
+            type="text"
+            placeholder="Прибытие"
+            className="search-form__input search-form__input--return"
+          />
+        </label>
 
-      <div className="search-form__select">select</div>
+        <div className="search-form__select">select</div>
 
-      <button type="submit" className="search-form__button">
-        Найти
-      </button>
-    </form>
+        <button type="submit" className="search-form__button">
+          Найти
+        </button>
+      </form>
+    </div>
   );
 };
 
