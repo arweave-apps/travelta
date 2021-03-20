9bd1b5c52df83a2401e04c4df5312a63875b6aa4
Sergey Vakhramov <vsa8813@gmail.com>
2021-03-21 04:41:19 +0500

    fix(hook): add condition and remove capture

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -13,15 +13,13 @@ export default function useOutsideClick(
     const handleClick = (e: MouseEvent) => {
       const target = e.target as Node;
 
-      if (!ref.current?.contains(target)) {
+      if (ref.current && !ref.current.contains(target)) {
         handler();
       }
     };
 
-    document.addEventListener('mousedown', handleClick, { capture: true });
+    document.addEventListener('mousedown', handleClick);
 
-    return () => {
-      document.removeEventListener('mousedown', handleClick, { capture: true });
-    };
+    return () => document.removeEventListener('mousedown', handleClick);
   }, [handler, isActive, ref]);
 }
