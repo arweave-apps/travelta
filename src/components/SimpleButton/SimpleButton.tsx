81a69568d284ca8ce0d43df387f5739a10c96117
Sergey Vakhramov <vsa8813@gmail.com>
2021-03-27 22:39:26 +0500

    refactor(SimpleButton): add accent and default styles

--- src/components/SimpleButton/SimpleButton.tsx
+++ src/components/SimpleButton/SimpleButton.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import './SimpleButton.scss';
 
-type SimpleButtonProps = { title: string; submit: boolean };
+type SimpleButtonProps = {
+  title: string;
+  submit?: boolean;
+  accent?: boolean;
+  second?: boolean;
+};
 
-const SimpleButton = ({ title, submit }: SimpleButtonProps): JSX.Element => {
+const SimpleButton = ({
+  title,
+  submit,
+  accent,
+  second,
+}: SimpleButtonProps): JSX.Element => {
   return (
-    <button type={submit ? 'submit' : 'button'} className="button">
+    <button
+      type={submit ? 'submit' : 'button'}
+      className={classNames('button', {
+        'button--accent': accent,
+        'button--second': second,
+      })}
+    >
       {title}
     </button>
   );
