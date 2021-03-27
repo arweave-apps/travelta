2a3eb04f18393b29cac3b7f0a7660ca7a54f75de
Sergey Vakhramov <vsa8813@gmail.com>
2021-03-27 22:40:24 +0500

    feat(redux): add pageSettings to redux

---
+++ src/redux/actions/pageSettings/types.ts
@@ -0,0 +1,8 @@
+export const SET_ACTIVE_FORM = 'SET_ACTIVE_FORM';
+
+type ActionSetACtiveFormType = {
+  type: typeof SET_ACTIVE_FORM;
+  payload: string;
+};
+
+export type ActionPageSettingsTypes = ActionSetACtiveFormType;
