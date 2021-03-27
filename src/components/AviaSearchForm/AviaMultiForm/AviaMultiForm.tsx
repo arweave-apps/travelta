90d67a35ad6a7d7e8fbb1aec71391b58aa7fe830
Sergey Vakhramov <vsa8813@gmail.com>
2021-03-27 23:47:40 +0500

    feat(multi form): add the functionality of adding segments

--- src/components/AviaSearchForm/AviaMultiForm/AviaMultiForm.tsx
+++ src/components/AviaSearchForm/AviaMultiForm/AviaMultiForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import useInput from '../../../hooks/useInput';
 
@@ -9,59 +9,58 @@ import Datepicker from '../../Datepicker';
 
 import './AviaMultiForm.scss';
 
+// const segments = [{ id: 'segment 1' }, { id: 'segment 2' }];
+
 const AviaMultiForm = (): JSX.Element => {
   const origin = useInput('');
   const destination = useInput('');
 
+  const [segments, setSegments] = useState([
+    { id: 'segment-1' },
+    { id: 'segment-2' },
+  ]);
+
+  const [segmentsCount, setSegmentsCount] = useState(segments.length);
+
+  const handleClickAddSegment = useCallback(() => {
+    if (segmentsCount > 5) {
+      return;
+    }
+
+    const newSegments = [...segments, { id: `segment-${segmentsCount + 1}` }];
+    setSegments(newSegments);
+    setSegmentsCount((prevState) => prevState + 1);
+  }, [segments, segmentsCount]);
+
   return (
     <form className="multicity-form">
-      <div className="multicity-form__segment">
-        <div className="multicity-form__origin">
-          <TextInput
-            placeholder="Откуда"
-            id="origin"
-            value={origin.value}
-            onChange={origin.onChange}
-          />
-        </div>
+      {segments.map(({ id }) => {
+        return (
+          <div className="multicity-form__segment" key={id}>
+            <div className="multicity-form__origin">
+              <TextInput
+                placeholder="Откуда"
+                id={`origin-${id}`}
+                value={origin.value}
+                onChange={origin.onChange}
+              />
+            </div>
 
-        <div className="multicity-form__destination">
-          <TextInput
-            placeholder="Куда"
-            id="destination"
-            value={destination.value}
-            onChange={destination.onChange}
-          />
-        </div>
+            <div className="multicity-form__destination">
+              <TextInput
+                placeholder="Куда"
+                id={`destination-${id}`}
+                value={destination.value}
+                onChange={destination.onChange}
+              />
+            </div>
 
-        <div className="multicity-form__datepicker">
-          <Datepicker />
-        </div>
-      </div>
-
-      <div className="multicity-form__segment">
-        <div className="multicity-form__origin">
-          <TextInput
-            placeholder="Откуда"
-            id="origin"
-            value={origin.value}
-            onChange={origin.onChange}
-          />
-        </div>
-
-        <div className="multicity-form__destination">
-          <TextInput
-            placeholder="Куда"
-            id="destination"
-            value={destination.value}
-            onChange={destination.onChange}
-          />
-        </div>
-
-        <div className="multicity-form__datepicker">
-          <Datepicker />
-        </div>
-      </div>
+            <div className="multicity-form__datepicker">
+              <Datepicker />
+            </div>
+          </div>
+        );
+      })}
 
       <div className="multicity-form__action">
         <div className="multicity-form__select">
@@ -69,7 +68,12 @@ const AviaMultiForm = (): JSX.Element => {
         </div>
 
         <div className="multicity-form__add-btn">
-          <SimpleButton second title="+ добавить перелёт" />
+          <SimpleButton
+            second
+            title="+ добавить перелёт"
+            onClick={handleClickAddSegment}
+            disabled={segmentsCount > 5}
+          />
         </div>
 
         <div className="multicity-form__search-btn">
