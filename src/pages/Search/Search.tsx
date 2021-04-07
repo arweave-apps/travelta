0cd01516286a11b765148dc9dfa5a3c066152185
Sergey Vakhramov <vsa8813@gmail.com>
2021-04-07 22:53:19 +0500

    feat(components): add the content of the search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import Prediction from '../../components/Prediction';
+import SearchAction from '../../components/SearchAction';
+import Ticket from '../../components/Ticket';
+
+import './Search.scss';
+
 const Search = (): JSX.Element => {
-  return <section>search</section>;
+  return (
+    <section className="avia-search">
+      <div className="container-small">
+        <div className="avia-search__inner">
+          <div className="filters">filters</div>
+          <Prediction />
+          <SearchAction />
+
+          <div className="tickets">
+            <Ticket />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Search;
