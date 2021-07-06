f1b074427a2a182a2526747a7b52ae8faefa0f02
Sergey Vakhramov <vsa8813@gmail.com>
2021-07-06 15:18:06 +0500

    refactor(components): change span to h1 tag

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -14,13 +14,13 @@ const Logo = ({ img, isSearchPage }: LogoProps): JSX.Element => {
   return (
     <NavLink className="logo" exact to="/">
       <div className="logo__img">{img}</div>
-      <span
+      <h1
         className={classNames('logo__title', {
           'logo__title--search': isSearchPage,
         })}
       >
         Travelta
-      </span>
+      </h1>
     </NavLink>
   );
 };
