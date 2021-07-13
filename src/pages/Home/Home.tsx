30e2d11df2e0231bcd343e9c8bd8be1aa2007718
Sergey Vakhramov <vsa8813@gmail.com>
2021-07-13 22:56:31 +0500

    feat: add best-flights layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,7 @@ import ItalyImg from '../../assets/images/destinations/italy.jpg';
 import JapanImg from '../../assets/images/destinations/japan.jpg';
 import MaldivesImg from '../../assets/images/destinations/maldives.jpg';
 import ThailandImg from '../../assets/images/destinations/thailand.jpg';
+
 import AwardIcon from '../../assets/images/icons/award.svg';
 import WorldwideIcon from '../../assets/images/icons/location.svg';
 import PriceIcon from '../../assets/images/icons/price-tag.svg';
@@ -15,10 +16,199 @@ import Air1 from '../../assets/images/offers/air-1.jpg';
 import Air2 from '../../assets/images/offers/air-2.jpg';
 import Air3 from '../../assets/images/offers/air-3.jpg';
 
+import PlaceParisImg from '../../assets/images/best-flights/paris.jpg';
+import PlaceLondonImg from '../../assets/images/best-flights/london.jpg';
+import PlaceAmsterdamImg from '../../assets/images/best-flights/amsterdam.jpg';
+import PlaceRomeImg from '../../assets/images/best-flights/rome.jpg';
+import PlaceBerlinImg from '../../assets/images/best-flights/berlin.jpg';
+import PlaceTokioImg from '../../assets/images/best-flights/tokio.jpg';
+import PlaceDubaiImg from '../../assets/images/best-flights/dubai2.jpg';
+import PlaceGoaImg from '../../assets/images/best-flights/goa.jpg';
+// eslint-disable-next-line max-len
+import PlaceSaintPetersburgImg from '../../assets/images/best-flights/saint-petersburg.jpg';
+import PlaceTorontoImg from '../../assets/images/best-flights/toronto.jpg';
+import PlaceNewYorkImg from '../../assets/images/best-flights/new-york.jpg';
+import PlaceGizaImg from '../../assets/images/best-flights/giza.jpg';
+
 import Icon from '../../components/Icon';
 import Layout from '../../components/Layout';
 
 import './Home.scss';
+
+const bestFlightCountriesBages = [
+  'Africa',
+  'United Arab Emirates',
+  'Canada',
+  'Russian Fedaration',
+  'United Kingdom',
+  'Unated States',
+  'Germany',
+  'Europe',
+  'Asia',
+  'India',
+  'North America',
+  'Oceania',
+  'Niderlands',
+];
+
+const bestFlightsPlaces = [
+  {
+    id: 'place-1',
+    image: PlaceParisImg,
+    location: {
+      country: 'France',
+      city: 'Paris',
+    },
+    route: {
+      from: 'SVX',
+      to: 'LFPG',
+    },
+    startprice: '25 654 ₽',
+  },
+  {
+    id: 'place-2',
+    image: PlaceLondonImg,
+    location: {
+      country: 'UK',
+      city: 'London',
+    },
+    route: {
+      from: 'SVX',
+      to: 'EOLL',
+    },
+    startprice: '20 675 ₽',
+  },
+  {
+    id: 'place-3',
+    image: PlaceAmsterdamImg,
+    location: {
+      country: 'Niderlands',
+      city: 'Amsterdam',
+    },
+    route: {
+      from: 'SVX',
+      to: 'EHAA',
+    },
+    startprice: '27 021 ₽',
+  },
+  {
+    id: 'place-4',
+    image: PlaceRomeImg,
+    location: {
+      country: 'Germany',
+      city: 'Berlin',
+    },
+    route: {
+      from: 'SVX',
+      to: 'BER',
+    },
+    startprice: '15 032 ₽',
+  },
+  {
+    id: 'place-5',
+    image: PlaceBerlinImg,
+    location: {
+      country: 'Japan',
+      city: 'Tokio',
+    },
+    route: {
+      from: 'SVX',
+      to: 'JST',
+    },
+    startprice: '50 467 ₽',
+  },
+  {
+    id: 'place-6',
+    image: PlaceTokioImg,
+    location: {
+      country: 'France',
+      city: 'Paris',
+    },
+    route: {
+      from: 'SVX',
+      to: 'LFPG',
+    },
+    startprice: '25 654 ₽',
+  },
+  {
+    id: 'place-7',
+    image: PlaceDubaiImg,
+    location: {
+      country: 'UAE',
+      city: 'Dubai',
+    },
+    route: {
+      from: 'SVX',
+      to: 'DXB',
+    },
+    startprice: '79 467 ₽',
+  },
+  {
+    id: 'place-8',
+    image: PlaceGoaImg,
+    location: {
+      country: 'India',
+      city: 'Goa',
+    },
+    route: {
+      from: 'SVX',
+      to: 'GOA',
+    },
+    startprice: '98 685 ₽',
+  },
+  {
+    id: 'place-9',
+    image: PlaceSaintPetersburgImg,
+    location: {
+      country: 'Russia',
+      city: 'Saint-Petersburg',
+    },
+    route: {
+      from: 'SVX',
+      to: 'LED',
+    },
+    startprice: '7 918 ₽',
+  },
+  {
+    id: 'place-10',
+    image: PlaceTorontoImg,
+    location: {
+      country: 'Canada',
+      city: 'Toronto',
+    },
+    route: {
+      from: 'SVX',
+      to: 'YZZ',
+    },
+    startprice: '122 984 ₽',
+  },
+  {
+    id: 'place-11',
+    image: PlaceNewYorkImg,
+    location: {
+      country: 'USA',
+      city: 'New York',
+    },
+    route: {
+      from: 'SVX',
+      to: 'EWR',
+    },
+    startprice: '147 398 ₽',
+  },
+  {
+    id: 'place-12',
+    image: PlaceGizaImg,
+    location: {
+      country: 'Egypt',
+      city: 'Giza',
+    },
+    route: {
+      from: 'SVX',
+      to: 'SPX',
+    },
+    startprice: '78 557 ₽',
+  },
+];
 
 const Home = (): JSX.Element => {
   return (
@@ -34,6 +224,7 @@ const Home = (): JSX.Element => {
                 Adipiscing morbi laoreet amet,posuere et, eget vivamus ac.
               </p>
             </div>
+
             <div className="site-info__item">
               <Icon icon={<AwardIcon />} className="site-info__icon" />
               <h3 className="site-info__title">Award Winning Service</h3>
@@ -42,6 +233,7 @@ const Home = (): JSX.Element => {
                 Adipiscing morbi laoreet amet,posuere et, eget vivamus ac.
               </p>
             </div>
+
             <div className="site-info__item">
               <Icon icon={<WorldwideIcon />} className="site-info__icon" />
               <h3 className="site-info__title">Worldwide Coverag</h3>
@@ -205,6 +397,63 @@ const Home = (): JSX.Element => {
           </div>
         </div>
       </Layout>
+
+      <Layout containerSize="small" tag="section" className="best-flights">
+        <div className="section-inner">
+          <h2 className="section-title">
+            Best Flight Deals of the Day{' '}
+            <span className="best-flights__selector">Yekaterinburg</span>
+          </h2>
+
+          <div className="best-flights__bages">
+            {bestFlightCountriesBages.map((country) => {
+              return (
+                <button
+                  key={country}
+                  type="button"
+                  className="best-flights__bage"
+                >
+                  {country}
+                </button>
+              );
+            })}
+          </div>
+
+          <div className="best-flights__items">
+            {bestFlightsPlaces.map(
+              ({ id, image, location, route, startprice }) => {
+                return (
+                  <div className="best-flights__item" key={id}>
+                    <img
+                      src={image}
+                      alt="country"
+                      className="best-flights__image"
+                    />
+
+                    <div className="best-flights__content">
+                      <h3 className="best-flights__title">
+                        {location.country}, {location.city}
+                      </h3>
+
+                      <div className="best-flights__action">
+                        <span className="best-flights__route">
+                          {route.from} - {route.to}
+                        </span>
+
+                        <a href="" className="best-flights__price-link">
+                          от {startprice}
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+        </div>
+      </Layout>
+
+      <Layout />
     </>
   );
 };
