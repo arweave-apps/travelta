343309807db5cde2b5f1e9dee09625fd63626566
Sergey Vakhramov <vsa8813@gmail.com>
2021-08-05 20:24:58 +0500

    feat(components): close dropdown on outside click & click on item

--- src/components/SearchAction/SearchAction.tsx
+++ src/components/SearchAction/SearchAction.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import DownArrowicon from '../../assets/images/icons/down-arrow.svg';
+import useOutsideClick from '../../hooks/useOutsideClick';
 import { sortTicketsByPrice } from '../../redux/actions/tickets/tickets';
 import { PriceSortTypes } from '../../redux/reducers/tickets';
 import { getSortTicketBy } from '../../selectors/selectors';
+
+import DownArrowicon from '../../assets/images/icons/down-arrow.svg';
+
 import Dropdown from '../Dropdown';
 import DropdownItem from '../Dropdown/DropdownItem/DropdownItem';
 import Icon from '../Icon';
@@ -31,7 +34,11 @@ const SearchAction = ({ totalTickets }: SearchActionsProps): JSX.Element => {
   const dispatch = useDispatch();
   const [isOpen, setOpen] = useState<boolean>(false);
 
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
   const sortedBy = useSelector(getSortTicketBy);
+
+  useOutsideClick(wrapperRef, () => setOpen(false), isOpen);
 
   const toggleDropdownMenu = () => {
     setOpen(!isOpen);
@@ -39,13 +46,14 @@ const SearchAction = ({ totalTickets }: SearchActionsProps): JSX.Element => {
 
   const handleSelectSort = (id: PriceSortTypes) => {
     dispatch(sortTicketsByPrice(id));
+    setOpen(false);
   };
 
   const selectedItemText = sortListsData.find((item) => item.id === sortedBy)
     ?.text;
 
   return (
-    <div className="action">
+    <div className="action" ref={wrapperRef}>
       <div className="action__total-tickets">Результатов {totalTickets}</div>
 
       <div className="action__sort">
